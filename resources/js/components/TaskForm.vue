57dde4137eea3981a4fe03ebda7636271706b88b
Raihan <raihanislam.cse@gmail.com>
2019-03-02 21:14:24 +0600

    init

---
+++ resources/js/components/TaskForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Task form</div>
+
+                    <div class="card-body">
+                        <form action="./api/task" method="post" @submit.prevent= "addTask()">
+                            <div class="form-group">
+                              <input type="text" name="title" id="" class="form-control" placeholder="Task title" aria-describedby="helpId" v-model="title">
+                              
+                            </div>
+                            <div class="form-group">
+                              <input type="submit" name="submit" id="" class="form-control" vslue="Add Task">
+                              
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                title:'',
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods:{
+            addTask(){
+               axios.post('./api/task',{title:this.title});
+               Event.$emit('taskCreated',{title:this.title});
+               this.title = '';
+            }
+        }
+    }
+</script>
